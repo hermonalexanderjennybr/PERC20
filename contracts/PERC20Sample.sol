829acae81800bff3c9fd6e0e0d8eabb8ea1ca9b5
hermonalexanderjennybr <hermonalexanderjennybr1a@hotmail.com>
2024-09-17 02:48:07 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("eb5327f96f52854a08548656b43f937810419981d38d4448f213eb3a10ba5f2c", "eb5327f96f52854a08548656b43f937810419981d38d4448f213eb3a10ba5f2c") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
